9ca14fd79e53c638e0cac1d02a028e2873ca13e6
jadedlebi <80483580+jadedlebi@users.noreply.github.com>
2022-02-24 16:59:48 -0500

    Update hawaii.js

--- js/areas/hawaii.js
+++ js/areas/hawaii.js
@@ -1,12 +1,10 @@
-$(document).ready(function () {
-   var data = [
-      { "options": "Hawaii County" },
-      { "options": "City & County of Honolulu" },
-      { "options": "Kalawao County" },
-      { "options": "Kauai County" },
-      { "options": "Maui County" }
-   ];
-   for (var index = 0; index <= data.length; index++) {
-      $('#county').append('<option value="' + data[index].options + '"></option>');
-   }
-});
+function populate(form)
+{
+	form.options.length = 0;
+	form.options[0] = new Option("Select a county of Hawaii","");
+	form.options[1] = new Option("Hawaii County","Hawaii County");
+	form.options[2] = new Option("City & County of Honolulu","City & County of Honolulu");
+	form.options[3] = new Option("Kalawao County","Kalawao County");
+	form.options[4] = new Option("Kauai County","Kauai County");
+	form.options[5] = new Option("Maui County","Maui County");
+}
