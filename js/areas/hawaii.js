7d2a6a8d1d6bcfcc60816d0d3d796b3996e8884c
jadedlebi <80483580+jadedlebi@users.noreply.github.com>
2022-03-29 13:25:18 -0400

    Update hawaii.js

--- js/areas/hawaii.js
+++ js/areas/hawaii.js
@@ -1,6 +1,6 @@
 function populate(form)
 {
-	let form.options = [];
+	let options = [];
 	form.options[0] = new Option("Select a county of Hawaii","");
 	form.options[1] = new Option("Hawaii County","Hawaii County");
 	form.options[2] = new Option("City & County of Honolulu","City & County of Honolulu");
