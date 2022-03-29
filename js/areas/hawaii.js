6840a901d1e5b6b35a31c02eb7250e00bc46814d
jadedlebi <80483580+jadedlebi@users.noreply.github.com>
2022-03-29 12:52:27 -0400

    added failsafe on undefined properties

--- js/areas/hawaii.js
+++ js/areas/hawaii.js
@@ -1,5 +1,6 @@
 function populate(form)
 {
+	if(form.options === undefined) {return}
 	form.options[0] = new Option("Select a county of Hawaii","");
 	form.options[1] = new Option("Hawaii County","Hawaii County");
 	form.options[2] = new Option("City & County of Honolulu","City & County of Honolulu");
