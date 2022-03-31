5177ec6dc4e75d2199098c71185d1af21dac187f
jadedlebi <80483580+jadedlebi@users.noreply.github.com>
2022-03-31 17:25:07 -0400

    Update tennessee.js

--- js/areas/tennessee.js
+++ js/areas/tennessee.js
@@ -1,7 +1,7 @@
 function populate(form)
 {
 form.options.length = 0;
-form.options[0] = new Option("Select a county of Tennessee","");
+form.options[0] = new Option("All counties in Tennessee","");
 form.options[1] = new Option("Anderson County","Anderson County");
 form.options[2] = new Option("Bedford County","Bedford County");
 form.options[3] = new Option("Benton County","Benton County");
