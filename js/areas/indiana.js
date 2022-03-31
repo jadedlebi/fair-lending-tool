74645f6f55fbe2d0ef5c141c6a1fc6842b4c48c5
jadedlebi <80483580+jadedlebi@users.noreply.github.com>
2022-03-31 17:09:04 -0400

    Update indiana.js

--- js/areas/indiana.js
+++ js/areas/indiana.js
@@ -1,7 +1,7 @@
 function populate(form)
 {
 form.options.length = 0;
-form.options[0] = new Option("Select a county of Indiana","");
+form.options[0] = new Option("All counties in Indiana","");
 form.options[1] = new Option("Adams County","Adams County");
 form.options[2] = new Option("Allen County","Allen County");
 form.options[3] = new Option("Bartholomew County","Bartholomew County");
