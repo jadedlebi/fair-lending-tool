72be7ad07f3b95e39c9fda1259215ba6b63cc784
jadedlebi <80483580+jadedlebi@users.noreply.github.com>
2022-03-31 17:11:57 -0400

    Update maine.js

--- js/areas/maine.js
+++ js/areas/maine.js
@@ -1,7 +1,7 @@
 function populate(form)
 {
 form.options.length = 0;
-form.options[0] = new Option("Select a county of Maine","");
+form.options[0] = new Option("All counties in Maine","");
 form.options[1] = new Option("Androscoggin County","Androscoggin County");
 form.options[2] = new Option("Aroostook County","Aroostook County");
 form.options[3] = new Option("Cumberland County","Cumberland County");
