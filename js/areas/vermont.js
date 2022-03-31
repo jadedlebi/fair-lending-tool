f3ee39689bffdaa8393428a6a64125ac838a9961
jadedlebi <80483580+jadedlebi@users.noreply.github.com>
2022-03-31 17:26:48 -0400

    Update vermont.js

--- js/areas/vermont.js
+++ js/areas/vermont.js
@@ -1,7 +1,7 @@
 function populate(form)
 {
 form.options.length = 0;
-form.options[0] = new Option("Select a county of Vermont",'');
+form.options[0] = new Option("All counties in Vermont",'');
 form.options[1] = new Option("Addison County","Addison County");
 form.options[2] = new Option("Bennington County","Bennington County");
 form.options[3] = new Option("Caledonia County","Caledonia County");
