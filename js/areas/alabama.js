17c9f68722db7edf321b1565ac84b03dba35ba20
jadedlebi <80483580+jadedlebi@users.noreply.github.com>
2022-03-31 17:03:37 -0400

    Update alabama.js

--- js/areas/alabama.js
+++ js/areas/alabama.js
@@ -1,7 +1,7 @@
 function populate(form)
 {
 form.options.length = 0;
-form.options[0] = new Option("Select a county of Alabama","");
+form.options[0] = new Option("All counties in Alabama","");
 form.options[1] = new Option("Autauga County","Autauga County");
 form.options[2] = new Option("Baldwin County","Baldwin County");
 form.options[3] = new Option("Barbour County","Barbour County");
