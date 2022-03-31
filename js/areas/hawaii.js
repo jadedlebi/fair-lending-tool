caead224d37839e7a47eb8f2c865527b0cbba260
jadedlebi <80483580+jadedlebi@users.noreply.github.com>
2022-03-31 15:03:06 -0400

    Update hawaii.js

--- js/areas/hawaii.js
+++ js/areas/hawaii.js
@@ -1,9 +1,9 @@
-function populate(form)
+function populate(forms)
 {
-	form = "Select a county of Hawaii";
-	form[0] = new Option("Hawaii County, Hawaii","Hawaii County, Hawaii");
-	form[1] = new Option("Honolulu County, Hawaii","Honolulu County, Hawaii");
-	form[2] = new Option("Kalawao County, Hawaii","Kalawao County, Hawaii");
-	form[3] = new Option("Kauai County, Hawaii","Kauai County, Hawaii");
-	form[4] = new Option("Maui County, Hawaii","Maui County, Hawaii");
+	forms.options = "Select a county of Hawaii";
+	forms.options[0] = new Option("Hawaii County, Hawaii","Hawaii County, Hawaii");
+	forms.options[1] = new Option("Honolulu County, Hawaii","Honolulu County, Hawaii");
+	forms.options[2] = new Option("Kalawao County, Hawaii","Kalawao County, Hawaii");
+	forms.options[3] = new Option("Kauai County, Hawaii","Kauai County, Hawaii");
+	forms.options[4] = new Option("Maui County, Hawaii","Maui County, Hawaii");
 }
