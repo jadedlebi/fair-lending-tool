70a9ed4c49b8c2f1c0a4f0f79f5eb969ec240397
jadedlebi <80483580+jadedlebi@users.noreply.github.com>
2022-03-31 17:07:29 -0400

    Update florida.js

--- js/areas/florida.js
+++ js/areas/florida.js
@@ -1,7 +1,7 @@
 function populate(form)
 {
 form.options.length = 0;
-form.options[0] = new Option("Select a county of Florida","");
+form.options[0] = new Option("All counties in Florida","");
 form.options[1] = new Option("Alachua County","Alachua County");
 form.options[2] = new Option("Baker County","Baker County");
 form.options[3] = new Option("Bay County","Bay County");
