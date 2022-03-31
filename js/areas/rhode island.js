c42be2fdb275d7430c64bb26bf52748602cbb9f1
jadedlebi <80483580+jadedlebi@users.noreply.github.com>
2022-03-31 17:24:10 -0400

    Update rhode island.js

--- js/areas/rhode island.js
+++ js/areas/rhode island.js
@@ -1,7 +1,7 @@
 function populate(form)
 {
 form.options.length = 0;
-form.options[0] = new Option("Select a county of Rhode Island","");
+form.options[0] = new Option("All counties in Rhode Island","");
 form.options[1] = new Option("Bristol County","Bristol County");
 form.options[2] = new Option("Kent County","Kent County");
 form.options[3] = new Option("Newport County","Newport County");
