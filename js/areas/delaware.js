02f2bdfbfa2447934050040da4e82a3d549712f2
jadedlebi <80483580+jadedlebi@users.noreply.github.com>
2022-03-31 17:07:02 -0400

    Update delaware.js

--- js/areas/delaware.js
+++ js/areas/delaware.js
@@ -1,7 +1,7 @@
 function populate(form)
 {
 form.options.length = 0;
-form.options[0] = new Option("Select a county of Delaware","");
+form.options[0] = new Option("All counties in Delaware","");
 form.options[1] = new Option("Kent County","Kent County");
 form.options[2] = new Option("New Castle County","New Castle County");
 form.options[3] = new Option("Sussex County","Sussex County");
