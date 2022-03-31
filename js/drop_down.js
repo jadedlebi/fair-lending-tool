263b89a0d7d49811b867ba870692f1a77009344c
jadedlebi <80483580+jadedlebi@users.noreply.github.com>
2022-03-31 17:49:39 -0400

    Update drop_down.js

--- js/drop_down.js
+++ js/drop_down.js
@@ -1,24 +1,20 @@
-function drop_down_list()
-{
-  var area = $('#area').val();
-    if(area == '')
-    {
-    $('#county_drop_down').hide();
-    $('#no_county_drop_down').show();
-    }
-    else
-    {
-      // Show the drop down
-      $('#no_county_drop_down').hide();
-      $('#county_drop_down').show();
-      $.getScript("https://jadedlebi.github.io/fair-lending-tool/" + area.toLowerCase() + ".js", function(){
-        populate(document.form.county);
-      });
+function drop_down_list() {
+    var state = $('#state').val();
+    if(state == '') {
+        $('#county_drop_down').hide();
+        $('#no_county_drop_down').show();
+    } else {
+	    $('#county_drop_down').show(); // Hide the drop down
+        $('#no_county_drop_down').hide(); // Hide the "no counties" message (if it's the case)
+        $.getScript("http://jadedlebi.github.io/fair-lending-tool/js/areas/"+ state.toLowerCase() + ".js", function(){
+            populate(document.form.county);
+	        $('#county_drop_down').show(); // Show the drop down
+        });
     }
 }
 
-$(document).ready(function(){
-  $("#area").change(drop_down_list);
+$(document).ready(function() {
+    $("#state").change(drop_down_list);
 });
 
 $(window).load(drop_down_list);
