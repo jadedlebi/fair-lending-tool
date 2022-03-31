357f4d1644f3f9c6cf3c3dce8375167c615e60a0
jadedlebi <80483580+jadedlebi@users.noreply.github.com>
2022-03-31 17:04:53 -0400

    Update arkansas.js

--- js/areas/arkansas.js
+++ js/areas/arkansas.js
@@ -1,7 +1,7 @@
 function populate(form)
 {
 form.options.length = 0;
-form.options[0] = new Option("Select a county of Arkansas","");
+form.options[0] = new Option("All counties in Arkansas","");
 form.options[1] = new Option("Arkansas County","Arkansas County");
 form.options[2] = new Option("Ashley County","Ashley County");
 form.options[3] = new Option("Baxter County","Baxter County");
