39bcb3c5251ad3bf90cf29595c9028475f1e72cb
jadedlebi <80483580+jadedlebi@users.noreply.github.com>
2022-04-15 13:17:40 -0400

    Update and rename aberdeen, sd.js to albany-schenectady-troy, ny.js

--- js/areas/aberdeen, sd.js
+++
@@ -1,7 +0,0 @@
-function populate(form)
-{
-form.options.length = 0;
-form.options[0] = new Option("All counties","");
-form.options[1] = new Option("Brown County","Brown County");
-form.options[2] = new Option("Edmunds County","Edmunds County");
-}
