48c870e4eaec2981af622414ff0fbe12ab8700df
jadedlebi <80483580+jadedlebi@users.noreply.github.com>
2022-05-05 09:47:43 -0400

    Changed values from county_state to geoid5

--- js/areas/10580.js
+++ js/areas/10580.js
@@ -2,9 +2,9 @@ function populate(form)
 {
 form.options.length = 0;
 form.options[0] = new Option("All counties","");
-form.options[1] = new Option("Albany County","Albany County");
-form.options[2] = new Option("Rensselaer County","Rensselaer County");
-form.options[3] = new Option("Saratoga County","Saratoga County");
-form.options[4] = new Option("Schenectady County","Schenectady County");
-form.options[5] = new Option("Schoharie County","Schoharie County");
+form.options[1] = new Option("Albany County","36001");
+form.options[2] = new Option("Rensselaer County","36083");
+form.options[3] = new Option("Saratoga County","36091");
+form.options[4] = new Option("Schenectady County","36093");
+form.options[5] = new Option("Schoharie County","36095");
 }
